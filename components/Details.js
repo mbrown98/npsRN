e09899cdc0f9ea8a9c654fd6caa28892e284394f
Matthew Brown <mbrown@geomarvel.com>
2022-03-11 16:29:22 -0500

    park page head content added

--- components/Details.js
+++ components/Details.js
@@ -1,10 +1,81 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import colors from '../assets/colors/colors';
+import Entypo from 'react-native-vector-icons/Entypo';
 
-export default function Details() {
+Entypo.loadFont();
+const height = Dimensions.get('window').height;
+
+export default function Details({
+  navigation,
+  route: {
+    params: {item},
+  },
+}) {
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={styles.container}>
+      <ImageBackground source={item.imageBig} style={styles.backgroundImage}>
+        <TouchableOpacity
+          style={styles.backIcon}
+          onPress={() => navigation.goBack()}>
+          <Entypo name="chevron-left" color={colors.white} size={32} />
+        </TouchableOpacity>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.itemTitle}>{item.title}</Text>
+          <View style={styles.locationWrapper}>
+            <Entypo name="location-pin" color={colors.white} size={24} />
+            <Text style={styles.locationText}>{item.location}</Text>
+          </View>
+        </View>
+      </ImageBackground>
+      <View style={styles.descriptionWrapper}></View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  backgroundImage: {
+    height: height * 0.6,
+    justifyContent: 'space-between',
+  },
+  descriptionWrapper: {
+    backgroundColor: colors.white,
+    flex: 1,
+    marginTop: -20,
+    borderRadius: 25,
+  },
+  backIcon: {
+    marginLeft: 20,
+    marginTop: 60,
+  },
+  titleWrapper: {
+    marginHorizontal: 20,
+    marginBottom: 40,
+  },
+  itemTitle: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 32,
+    color: colors.white,
+  },
+  locationWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  locationText: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 16,
+    color: colors.white,
+  },
+});
