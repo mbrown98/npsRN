2ecd60896fbcc9c613b6172513da50b614514e29
Matthew Brown <mbrown@geomarvel.com>
2022-03-10 21:40:50 -0500

    scroll cards adde

--- components/Home.js
+++ components/Home.js
@@ -7,18 +7,42 @@ import {
   ScrollView,
   SafeAreaView,
   Image,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
 } from 'react-native';
 import colors from '../assets/colors/colors';
 import profile from '../assets/images/person.png';
 import Feather from 'react-native-vector-icons/Feather';
+import Entypo from 'react-native-vector-icons/Entypo';
 import activitiesData from '../assets/data/activitiesData';
 import discoverCategoriesData from '../assets/data/discoverCategoriesData';
 import learnMoreData from '../assets/data/learnMoreData';
 import discoverData from '../assets/data/discoverData';
 
 Feather.loadFont();
+Entypo.loadFont();
 
 export default function Home({navigation}) {
+  const renderDiscoveryItem = ({item}) => (
+    <TouchableOpacity
+      onPress={() => navigation.navigate('Details', {item: item})}>
+      <ImageBackground
+        source={item.image}
+        style={[
+          styles.discoverItem,
+          {marginLeft: item.id === 'discover-1' ? 20 : 0},
+        ]}
+        imageStyle={styles.discoverItemImage}>
+        <Text style={styles.discoverItemTitle}>{item.title}</Text>
+        <View style={styles.discoverItemLocationWrapper}>
+          <Entypo name="location-pin" size={18} color={colors.white} />
+          <Text style={styles.discoverItemLocationText}>{item.location}</Text>
+        </View>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
       <ScrollView>
@@ -46,6 +70,16 @@ export default function Home({navigation}) {
             <Text style={styles.discoverCategoriesText}>Cities</Text>
             <Text style={styles.discoverCategoriesText}>Experiences</Text>
           </View>
+
+          <View style={styles.discoverItemsWrapper}>
+            <FlatList
+              data={discoverData}
+              renderItem={renderDiscoveryItem}
+              keyExtractor={item => item.id}
+              horizontal
+              showScroll={false}
+            />
+          </View>
         </View>
       </ScrollView>
     </View>
@@ -71,15 +105,17 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   discoverWrapper: {
-    marginHorizontal: 20,
+    // marginHorizontal: 20,
     marginTop: 20,
   },
   discoverTitle: {
+    marginHorizontal: 20,
     fontFamily: 'Lato-Bold',
     fontSize: 32,
   },
 
   discoverCategoriesWrapper: {
+    marginHorizontal: 20,
     flexDirection: 'row',
     marginTop: 20,
   },
@@ -87,5 +123,35 @@ const styles = StyleSheet.create({
     marginRight: 30,
     fontFamily: 'Lato-Regular',
     color: colors.gray,
+  },
+  discoverItemsWrapper: {
+    paddingVertical: 20,
+  },
+  discoverItem: {
+    width: 170,
+    height: 250,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    marginRight: 20,
+  },
+  discoverItemImage: {
+    borderRadius: 20,
+  },
+  discoverItemTitle: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 18,
+    color: colors.white,
+  },
+  discoverItemLocationWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  discoverItemLocationText: {
+    marginLeft: 5,
+    fontFamily: 'Lato-Bold',
+    fontSize: 14,
+    color: colors.white,
   },
 });
