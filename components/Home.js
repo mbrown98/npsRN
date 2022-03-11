7764fe494e0643038aea7e7c43c425a5f5045189
Matthew Brown <mbrown@geomarvel.com>
2022-03-10 21:07:36 -0500

    imports for home

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import colors from '../assets/colors/colors';
+import Feather from 'react-native-vector-icons/Feather';
+import activitiesData from '../assets/data/activitiesData';
+import discoverCategoriesData from '../assets/data/discoverCategoriesData';
+import learnMoreData from '../assets/data/learnMoreData';
+import discoverData from '../assets/data/discoverData';
 
 export default function Home({navigation}) {
   return (
-    <View>
-      <Text>Home</Text>
-      <Button
-        onPress={() => navigation.navigate('Details')}
-        title="Click Me!"
-      />
+    <View style={styles.container}>
+      <Text>Hello</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: colors.white,
+  },
+});
