2d6dd2974eba29128247b559a49fd0a0507d6950
Matthew Brown <mbrown@geomarvel.com>
2022-03-13 21:06:29 -0400

    onboard folder added

---
+++ pages/Onboard/Onboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, Dimensions} from 'react-native';
+
+const w = Dimensions.get('window').width;
+const h = Dimensions.get('window').height;
+
+export default function Onboard() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
