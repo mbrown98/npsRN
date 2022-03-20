2c893ba16d9791c7840cec3dfe4c8e68b161e4b2
Matthew Brown <mbrown@geomarvel.com>
2022-03-20 19:55:10 -0400

    simple login page added

--- pages/Login/Login.js
+++ pages/Login/Login.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, Button} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+async function onGoogleButtonPress() {
+  // Get the users ID token
+  const {idToken} = await GoogleSignin.signIn();
+
+  // Create a Google credential with the token
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+  // Sign-in the user with the credential
+  const user_sign_in = auth().signInWithCredential(googleCredential);
+
+  user_sign_in
+    .then(res => console.log('resss:', res))
+    .catch(e => console.log('e', e));
+}
 
 export default function Login() {
   return (
     <SafeAreaView>
-      <Text>Login</Text>
+      <Button
+        title="Google Sign-In"
+        onPress={() =>
+          onGoogleButtonPress().then(() =>
+            console.log('Signed in with Google!'),
+          )
+        }
+      />
     </SafeAreaView>
   );
 }
