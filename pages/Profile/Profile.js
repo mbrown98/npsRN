86befe230955da6efc90f182672954b06ef05665
Matthew Brown <mbrown@geomarvel.com>
2022-03-21 18:54:12 -0400

    all park codes added

--- pages/Profile/Profile.js
+++ pages/Profile/Profile.js
@@ -3,10 +3,30 @@ import {View, Text, SafeAreaView, Button} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {useAuth} from '../../context/auth-context';
 import {getFavoriteParks} from '../../api/firebase/parks';
+import axios from 'axios';
 
 export default function Profile() {
   const {user} = useAuth();
   console.log(user.uid);
+
+  const getAllParks = async () => {
+    axios
+      .get(
+        'https://developer.nps.gov/api/v1/parks?limit=500&api_key=xVDrllRsZGSuU1sLpzu687U6R8bZG9NpU4W2wwSM',
+      )
+      .then(res => {
+        // console.log(res.data);
+
+        const data = res.data.data;
+        if (data) console.log('data', typeof data);
+        console.log(
+          data.map(opt => ({
+            parkCode: opt.parkCode,
+            fullName: opt.fullName,
+          })),
+        );
+      });
+  };
   return (
     <SafeAreaView>
       <Button
@@ -18,6 +38,7 @@ export default function Profile() {
         }
       />
       <Button title="Get Favs" onPress={() => getFavoriteParks()} />
+      <Button title="Get All Parks" onPress={() => getAllParks()} />
       <Text>{JSON.stringify(user.uid)}</Text>
     </SafeAreaView>
   );
