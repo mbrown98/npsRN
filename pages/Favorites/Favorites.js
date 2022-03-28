af603c3d97c7f2372fa21886fae70b41bf760071
Matthew Brown <mbrown@geomarvel.com>
2022-03-28 15:41:56 -0400

    mapping out fav cards

--- pages/Favorites/Favorites.js
+++ pages/Favorites/Favorites.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 
 export default function Favorites() {
   return (
-    <SafeAreaView>
-      <Text>Liked</Text>
+    <SafeAreaView style={styles.contain}>
+      <FlatList
+        contentContainerStyle={styles.flatlistContent}
+        data={[{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}]}
+        numColumns={2}
+        renderItem={({item}) => (
+          <View style={styles.favCardWrapper}>
+            <Text>{item.id}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.id}
+      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  contain: {
+    flex: 1,
+  },
+  flatlistContent: {
+    flexGrow: 1,
+  },
+  favCardWrapper: {
+    flex: 0.5,
+    margin: 5,
+    backgroundColor: '#ddd',
+    height: 150,
+  },
+});
